2205974b88cf9c68de4181cebb2b9e665c5e653a
Michael Williams <manicdesigner@gmail.com>
2020-01-03 15:57:48 -0500

    FK = on

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/articles.db3'
-    }
+      filename: './data/articles.db3',
+    },
   },
-
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    },
+  },
 };
