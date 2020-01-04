41fdf96d8dd7f0ad86b0774a2860585224059e11
Mikey Williams <manicdesigner@gmail.com>
2020-01-03 22:46:54 -0500

    add usersRouter

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const usersRouter = require('./API/users/usersRouter');
+
 const server = express();
 
 server.use(cors());
@@ -11,6 +13,8 @@ server.use(express.json());
 server.use('/', (req, res) => {
     res.send('Api is up and running');
 })
+
+server.use('/users', usersRouter);
 
 module.exports = server;
 
