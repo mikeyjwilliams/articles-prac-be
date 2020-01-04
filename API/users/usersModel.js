d16b37c523b4c8d7b426a83097e2219e49a460a1
Mikey Williams <manicdesigner@gmail.com>
2020-01-03 23:36:59 -0500

    changed name for convention

--- API/users/usersModel.js
+++ API/users/usersModel.js
@@ -1,7 +1,7 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-    find,
+    locate,
     findById,
     addUser,
     remove,
@@ -9,7 +9,7 @@ module.exports = {
 
 }
 
-function find(query = {}) {
+function locate(query = {}) {
     const { sortby = 'user_id', sortdir = 'asc' } = query;
     return db('users').orderBy(sortby, sortdir).select('email', 'text', 'created_at', 'updated_at', 'user_id');
 
