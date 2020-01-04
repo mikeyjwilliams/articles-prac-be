2a16d808c42715d6e164ebb421c6299ac005ee2e
Michael Williams <manicdesigner@gmail.com>
2020-01-04 12:29:12 -0500

    findAll, findById, addCategory functions to categoryModel

--- API/categories/categoryModel.js
+++ API/categories/categoryModel.js
@@ -1,5 +1,23 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
+  findAll,
+  findById,
+  addCategory,
+};
 
+function findAll() {
+  return db('categories').select('categories');
+}
+
+function findById(id) {
+  return db('categories')
+    .where({ category_id: id })
+    .select('category');
+}
+
+async function addCategory(category) {
+  const [id] = await db('categories').insert(category);
+
+  return findById(id);
 }
