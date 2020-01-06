c3d095b1d0bc151addc5faf6b7b9d8b6647b09cf
Michael Williams <manicdesigner@gmail.com>
2020-01-06 11:58:59 -0500

    mod to remove fk's just keeping it simple

--- migrations/20200103211816_categories.js
+++ migrations/20200103211816_categories.js
@@ -1,14 +1,14 @@
-
-exports.up = async function (knex) {
-    return knex.schema.createTable('categories', table => {
-        table.increments('category_id');
-        table.string('category', 35).notNullable().unique();
-        table.timestamps(true, true);
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('user_id').references('users.user_id');
-    })
+exports.up = async function(knex) {
+  return knex.schema.createTable('categories', table => {
+    table.increments('category_id');
+    table
+      .string('category', 35)
+      .notNullable()
+      .unique();
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = async function (knex) {
-    return knex.schema.dropTableIfExists('categories');
+exports.down = async function(knex) {
+  return knex.schema.dropTableIfExists('categories');
 };
