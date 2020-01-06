7e74653030ab63332cff330021068b87778830c6
Michael Williams <manicdesigner@gmail.com>
2020-01-06 11:59:32 -0500

    rm ser_id FK from articles migration

--- migrations/20200103214116_articles.js
+++ migrations/20200103214116_articles.js
@@ -1,17 +1,20 @@
-
-exports.up = async function (knex) {
-    return knex.schema.createTable('articles', table => {
-        table.increments('article_id');
-        table.string('title', 160).notNullable().unique();
-        table.text('content').notNullable();
-        table.timestamps(true, true);
-        table.integer('category_id').unsigned().notNullable();
-        table.integer('user_id').unsigned().notNullable();
-        table.foreign('category_id').references('categories.category_id');
-        table.foreign('user_id').references('users.user_id');
-    })
+exports.up = async function(knex) {
+  return knex.schema.createTable('articles', table => {
+    table.increments('article_id');
+    table
+      .string('title', 160)
+      .notNullable()
+      .unique();
+    table.text('content').notNullable();
+    table.timestamps(true, true);
+    table
+      .integer('category_id')
+      .unsigned()
+      .notNullable();
+    table.foreign('category_id').references('categories.category_id');
+  });
 };
 
-exports.down = async function (knex) {
-    return knex.schema.dropIfTableExists('articles');
+exports.down = async function(knex) {
+  return knex.schema.dropTableIfExists('articles');
 };
